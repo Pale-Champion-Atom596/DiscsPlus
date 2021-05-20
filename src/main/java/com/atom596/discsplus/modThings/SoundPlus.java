8135352a9bf5f4c56f2332e788c700515577d90d
Pale-Champion-Atom596 <adamhoffensetz@gmail.com>
2021-05-20 11:13:53 -0400

    Made discs play music

--- src/main/java/com/atom596/discsplus/modThings/SoundPlus.java
+++ src/main/java/com/atom596/discsplus/modThings/SoundPlus.java
@@ -6,11 +6,11 @@ import net.minecraft.util.Identifier;
 import net.minecraft.util.registry.Registry;
 
 public class SoundPlus {
-    public static final SoundEvent MUSIC_DISC_ANTI = register("anti");
-    public static final SoundEvent MUSIC_DISC_CHOP = register("chop");
-    public static final SoundEvent MUSIC_DISC_RANGE = register("range");
-    public static final SoundEvent MUSIC_DISC_PILLAGED = register("pillaged");
-    public static final SoundEvent MUSIC_DISC_TALL = register("tall");
+    public static final SoundEvent MUSIC_DISC_ANTI = register("music_disc_anti");
+    public static final SoundEvent MUSIC_DISC_CHOP = register("music_disc_chop");
+    public static final SoundEvent MUSIC_DISC_RANGE = register("music_disc_range");
+    public static final SoundEvent MUSIC_DISC_PILLAGED = register("music_disc_pillaged");
+    public static final SoundEvent MUSIC_DISC_TALL = register("music_disc_tall");
 
     private static SoundEvent register(String name) {
         Identifier id = new Identifier(DiscsPlus.MOD_ID, name);
