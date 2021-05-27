2c3f09615b9ebcf7e1df649c67de9b23935dd9c4
Pale-Champion-Atom596 <adamhoffensetz@gmail.com>
2021-05-27 17:46:16 -0400

    Added New Discs
    Added Broken Sky, Shulk, Shock, and Free the End.

--- src/main/java/com/atom596/discsplus/modThings/MusicDiscPlus.java
+++ src/main/java/com/atom596/discsplus/modThings/MusicDiscPlus.java
@@ -16,6 +16,11 @@ public class MusicDiscPlus extends MusicDiscItem {
     public static Item MUSIC_DISC_RANGE;
     public static Item MUSIC_DISC_PILLAGED;
     public static Item MUSIC_DISC_TALL;
+    public static Item MUSIC_DISC_BROKEN_SKY;
+    public static Item MUSIC_DISC_SHULK;
+    public static Item MUSIC_DISC_SHOCK;
+    public static Item MUSIC_DISC_FREE_THE_END;
+
     private static final Item.Settings SETTINGS = new Item.Settings().maxCount(1).group(ItemGroup.MISC)
             .rarity(Rarity.RARE);
 
@@ -29,6 +34,11 @@ public class MusicDiscPlus extends MusicDiscItem {
         MUSIC_DISC_RANGE = register("music_disc_range", new MusicDiscPlus(SoundPlus.MUSIC_DISC_RANGE, SETTINGS));
         MUSIC_DISC_PILLAGED = register("music_disc_pillaged", new MusicDiscPlus(SoundPlus.MUSIC_DISC_PILLAGED, SETTINGS));
         MUSIC_DISC_TALL = register("music_disc_tall", new MusicDiscPlus(SoundPlus.MUSIC_DISC_TALL, SETTINGS));
+        MUSIC_DISC_BROKEN_SKY = register("music_disc_broken_sky", new MusicDiscPlus(SoundPlus.MUSIC_DISC_BROKEN_SKY, SETTINGS));
+        MUSIC_DISC_SHULK = register("music_disc_shulk", new MusicDiscPlus(SoundPlus.MUSIC_DISC_SHULK, SETTINGS));
+        MUSIC_DISC_SHOCK = register("music_disc_shock", new MusicDiscPlus(SoundPlus.MUSIC_DISC_SHOCK, SETTINGS));
+        MUSIC_DISC_FREE_THE_END = register("music_disc_free_the_end", new MusicDiscPlus(SoundPlus.MUSIC_DISC_FREE_THE_END, SETTINGS));
+
     }
 
     private static Item register(String name, Item item) {
--- src/main/java/com/atom596/discsplus/modThings/SoundPlus.java
+++ src/main/java/com/atom596/discsplus/modThings/SoundPlus.java
@@ -11,6 +11,10 @@ public class SoundPlus {
     public static final SoundEvent MUSIC_DISC_RANGE = register("music_disc_range");
     public static final SoundEvent MUSIC_DISC_PILLAGED = register("music_disc_pillaged");
     public static final SoundEvent MUSIC_DISC_TALL = register("music_disc_tall");
+    public static final SoundEvent MUSIC_DISC_BROKEN_SKY = register("music_disc_broken_sky");
+    public static final SoundEvent MUSIC_DISC_SHULK = register("music_disc_shulk");
+    public static final SoundEvent MUSIC_DISC_SHOCK = register("music_disc_shock");
+    public static final SoundEvent MUSIC_DISC_FREE_THE_END = register("music_disc_free_the_end");
 
     private static SoundEvent register(String name) {
         Identifier id = new Identifier(DiscsPlus.MOD_ID, name);
